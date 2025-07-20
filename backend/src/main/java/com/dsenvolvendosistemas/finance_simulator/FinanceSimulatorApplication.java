0233a1f415562e5e0d51ea306c04d134cc6415bc
Rafael Calixto <calixxtorafaell@gmail.com>
2025-07-20 09:33:29 -0300

    setup backend

---
+++ backend/src/main/java/com/dsenvolvendosistemas/finance_simulator/FinanceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.dsenvolvendosistemas.finance_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSimulatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dsenvolvendosistemas/finance_simulator/FinanceSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsenvolvendosistemas.finance_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
